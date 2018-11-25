8a840e76078e11405b09d1a5f7ae9b564315712b
Ashequl25 <ashequl.sust@gmail.com>
2018-11-24 20:15:05 -0500

    Uploaded selenium excel read file

---
+++ ReadExcelData.java
@@ -0,0 +1,20 @@
+package test.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelData {
+	public static XSSFSheet readData() throws IOException {
+		File src = new File("C:\\Users\\user\\Desktop\\Test Data.xlsx");
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet1 = wb.getSheetAt(0);
+		return sheet1;
+
+	}
+
+}
